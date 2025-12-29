fc57f6f6c4dd51e62dbc415ba58334a6e99a63c1
Zaid <zizogithub@gmail.com>
2025-12-29 21:14:44 +0300

    main file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,46 @@
+import antlr.Example1Lexer;
+import antlr.Example1Parser;
 
+import ast.flask.FlaskProgramNode;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import visitor.ASTBuilder;
+import visitor.ASTPrinter;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws Exception {
 
-      System.out.print("Ahmad");
+        // 1. Read source file
+        String source = Files.readString(
+                Paths.get("src/Test/test.txt")
+        );
+
+        // 2. Create ANTLR input
+        CharStream input = CharStreams.fromString(source);
+
+        // 3. Lexer
+        Example1Lexer lexer = new Example1Lexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // 4. Parser
+        Example1Parser parser = new Example1Parser(tokens);
+
+        // 5. Parse
+        ParseTree tree = parser.program();
+
+        // 6. Build AST (⚠️ يرجّع FlaskProgramNode مباشرة)
+        ASTBuilder builder = new ASTBuilder();
+        FlaskProgramNode program =
+                (FlaskProgramNode) builder.visit(tree);
+
+        // 7. Print AST
+        ASTPrinter printer = new ASTPrinter();
+        program.accept(printer);
     }
 }
